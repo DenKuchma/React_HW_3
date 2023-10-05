b37cb489be97608da311f38a68cf72551f9e1169
DenKuchma <den.kuchma10@gmail.com>
2023-10-05 15:09:58 +0300

    hw_3

--- src/components/Body/index.jsx
+++ src/components/Body/index.jsx
@@ -1,17 +1,11 @@
-import  Carousel  from '../Carousel';
+import { Taimer } from '../Taimer';
 import styles from './Body.module.css'
 
 export const Body = () => {
 
     return (
         <div className={styles.main}>
-            <Carousel  images={[
-            "https://ukrainetrek.com/blog/wp-content/uploads/2016/12/top-10-photos-ukrainian-nature-2016-1.jpg",
-            "https://ukrainetrek.com/blog/wp-content/uploads/2016/12/top-10-photos-ukrainian-nature-2016-2.jpg",
-            "https://ukrainetrek.com/blog/wp-content/uploads/2016/12/top-10-photos-ukrainian-nature-2016-3.jpg",
-            "https://ukrainetrek.com/blog/wp-content/uploads/2016/12/top-10-photos-ukrainian-nature-2016-4.jpg",
-            "https://ukrainetrek.com/blog/wp-content/uploads/2016/12/top-10-photos-ukrainian-nature-2016-5.jpg"
-            ]}/>
+            <Taimer/>
         </div>
     );
 };
